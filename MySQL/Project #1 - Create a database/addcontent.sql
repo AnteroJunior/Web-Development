21d96618ae65108dc41508076df0c15e490e8af3
Antero Júnior <anterojunior1530@gmail.com>
2021-04-10 00:30:41 -0300

    Add files via upload

---
+++ MySQL/Project #1 - Create a database/addcontent.sql
@@ -0,0 +1,11 @@
+INSERT INTO usuario VALUES(1, 'maria@teste.com', '12345', 'Maria', 'Silva', 'F');
+INSERT INTO usuario VALUES(2, 'antero@teste.com', '12345', 'Antero', 'Junior', 'M');
+INSERT INTO usuario VALUES(3, 'jordan@teste.com', '12345', 'Jordan', 'Antonio', 'M');
+
+INSERT INTO postagem VALUES(1, 'Teste 1', 'O Nordeste é quente', 1);
+INSERT INTO postagem VALUES(2, 'Teste 2', 'Sou aluno da UFAL', 2);
+INSERT INTO postagem VALUES(3, 'Teste 3', 'Eu moro em Maceió', 3);
+
+INSERT INTO comentario VALUES(1, 'Isso é verdade!', 1);
+INSERT INTO comentario VALUES(2, 'A UFAL é a melhor Universidade de Alagoas', 2);
+INSERT INTO comentario VALUES(3, 'Maceió tem as praias mais bonitas do Brasil', 3);
---
+++ MySQL/Project #1 - Create a database/queries.sql
@@ -0,0 +1,20 @@
+SELECT 
+  * 
+FROM
+  postagem
+WHERE
+  autor = 1;
+  
+  
+SELECT 
+  * 
+FROM
+  usuario;
+  
+
+SELECT 
+  * 
+FROM
+  comentario
+WHERE
+  id_comentario = 2;
